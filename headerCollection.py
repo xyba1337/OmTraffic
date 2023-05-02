9ef283249c85e6284ae17d745689640e8ba83518
xyba <62798507+xyba1337@users.noreply.github.com>
2023-05-02 21:03:48 +0200

    Add files via upload

---
+++ headerCollection.py
@@ -0,0 +1,30 @@
+plainHeaders = {
+    'accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+    'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'referrer': 'https://www.omegle.com/',
+    'referrerPolicy': 'strict-origin-when-cross-origin'
+}
+
+jsonHeaders = {
+  'accept': 'application/json',
+  'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+  'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  'sec-ch-ua': '\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '\"Windows\"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'referrer': 'https://www.omegle.com/',
+  'referrerPolicy': 'strict-origin-when-cross-origin',
+  'method': 'POST',
+  'mode': 'cors',
+  'credentials': 'omit'
+}
